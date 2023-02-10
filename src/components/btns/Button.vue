8e3b0849ded7e41039d037ca37d95ded3c4302b3
isacfg <isaac.lima.105@ufrn.edu.br>
2023-02-10 14:26:41 -0300

    #0.2.1v Socials Hero

--- src/components/btns/Button.vue
+++ src/components/btns/Button.vue
@@ -1,8 +1,8 @@
 <template>
   <a
-    class="transition-custom btn rounded-3xl border-purple bg-purple px-4 text-base font-semibold capitalize text-white outline-none hover:scale-110 max-md:text-sm"
+    class="transition-custom btn rounded-3xl border-purple bg-purple px-4 text-base font-semibold capitalize text-purple outline-none hover:scale-110 max-md:text-sm"
     :class="{
-      'px-6 hover:border-purple hover:bg-white hover:text-purple':
+      'px-6 text-white hover:border-purple hover:bg-white hover:text-purple':
         type === 'btn-primary',
       'border-2 bg-white text-purple hover:border-purple hover:bg-purple hover:text-white':
         type === 'btn-secondary',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,47 +1,78 @@
 <template>
+  <!-- NAVBAR -->
   <Header />
 
   <!-- hero -->
-  <div
-    class="container mx-auto flex h-screen flex-row items-center px-4 max-md:flex-col">
-    <div class="left flex flex-col gap-y-24">
-      <div class="main flex flex-col gap-y-7">
-        <div class="texts">
-          <p class="text-2xl font-medium text-black max-md:text-xl">
-            oii! meu nome é
-          </p>
-          <h1 class="text-8xl font-semibold text-purple max-md:text-7xl">
-            Isaac F.
-          </h1>
+  <div class="container mx-auto mt-14">
+    <div class="flex flex-row items-center px-4 max-md:flex-col">
+      <div class="left flex flex-col gap-y-24">
+        <div class="main flex flex-col gap-y-7">
+          <div class="texts">
+            <p class="text-2xl font-medium text-black max-md:text-xl">
+              oii! meu nome é
+            </p>
+            <h1 class="text-8xl font-semibold text-purple max-md:text-7xl">
+              Isaac F.
+            </h1>
+          </div>
+          <div class="btns flex items-center gap-x-24 max-md:gap-x-10">
+            <Button text="Currículo" type="btn-primary" />
+            <Button text="ver projetos" type="btn-secondary" />
+          </div>
         </div>
-        <div class="btns flex items-center gap-x-24 max-md:gap-x-10">
-          <Button text="Currículo" type="btn-primary" />
-          <Button text="ver projetos" type="btn-secondary" />
+        <div class="texts-b">
+          <p class="text-xl font-normal max-md:text-center max-md:text-base">
+            👩🏻‍💻 Trabalhando como frontend developer na
+            <a
+              href="https://www.ejectufrn.com.br/"
+              class="transition-custom text-purple hover:scale-110"
+              target="_blank"
+              >@EJECT</a
+            >
+          </p>
+          <p class="text-xl font-normal max-md:text-center max-md:text-base">
+            📚 Estudando Ciências e Tecnologia na
+            <a
+              class="transition-custom text-purple hover:scale-110"
+              target="_blank"
+              href="https://www.ufrn.br/"
+              >@UFRN</a
+            >
+          </p>
         </div>
       </div>
-      <div class="texts-b">
-        <p class="text-xl font-normal max-md:text-center max-md:text-base">
-          👩🏻‍💻 Trabalhando como frontend developer na
-          <a
-            href="https://www.ejectufrn.com.br/"
-            class="transition-custom text-purple hover:scale-110"
-            target="_blank"
-            >@EJECT</a
-          >
-        </p>
-        <p class="text-xl font-normal max-md:text-center max-md:text-base">
-          📚 Estudando Ciências e Tecnologia na
-          <a
-            class="transition-custom text-purple hover:scale-110"
-            target="_blank"
-            href="https://www.ufrn.br/"
-            >@UFRN</a
-          >
-        </p>
+      <div class="right ml-auto max-md:mx-auto max-md:my-auto">
+        <img src="../assets/ilus.svg" class="max-md:w-3/4" alt="" srcset="" />
       </div>
     </div>
-    <div class="right ml-auto max-md:mx-auto max-md:my-auto">
-      <img src="../assets/ilus.svg" class="max-md:w-3/4" alt="" srcset="" />
+    <!-- SOCIALS -->
+    <div
+      class="socials container mx-auto mt-16 flex w-full items-center gap-x-6 px-4">
+      <div class="line flex-1 border border-black"></div>
+      <div class="socials-icons flex gap-x-6">
+        <a href="">
+          <img
+            src="../assets/icons/behance-light.svg"
+            class=" max-md:w-6"
+            alt=""
+            srcset="" />
+        </a>
+        <a href="">
+          <img
+            src="../assets/icons/github-light.svg"
+            class="max-md:w-6"
+            alt=""
+            srcset="" />
+        </a>
+        <a href="">
+          <img
+            src="../assets/icons/linkedin-light.svg"
+            class="max-md:w-6"
+            alt=""
+            srcset="" />
+        </a>
+      </div>
+      <div class="line flex-1 border border-black"></div>
     </div>
   </div>
 </template>
