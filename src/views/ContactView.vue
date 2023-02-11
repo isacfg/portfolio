a2f5dfc9c78eedfe49a536ab93028d7da290fe2c
isacfg <isaac.lima.105@ufrn.edu.br>
2023-02-11 17:39:36 -0300

    Contact Page + Project page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,23 +1,24 @@
 <template>
   <Header />
-  <div class="about container mx-auto flex flex-row px-12 max-md:flex-col">
+  <div
+    class="container mx-auto mt-24 flex flex-row px-24 max-md:flex-col max-md:px-4">
     <div class="left w-3/5 max-md:w-full">
       <div class="text">
         <h1 class="text-5xl font-semibold text-black">
           Meu nome é <span class="text-purple">Isaac</span> 👋😊
         </h1>
-        <p class="font-regular text-base leading-7 text-black">
+        <p class="font-regular mt-4 text-base leading-7 text-black">
           I’m a software engineer specializing in building (and occasionally
           designing) exceptional digital experiences . Currently, I’m focused on
           building accessible, human-centered produts at Upstatement.
         </p>
-        <p class="font-regular text-base leading-7 text-black">
+        <p class="font-regular mt-4 text-base leading-7 text-black">
           I’m a software engineer specializing in building (and occasionally
           designing) exceptional digital experiences . Currently, I’m focused on
           building accessible, human-centered produts at Upstatement.
         </p>
       </div>
-      <div class="btns-socials flex items-center justify-between">
+      <div class="btns-socials mt-8 flex items-center justify-between">
         <div class="btns">
           <Button text="currículo" type="btn-secondary" />
         </div>
@@ -55,7 +56,7 @@
         </div>
       </div>
     </div>
-    <div class="right ml-auto">
+    <div class="right bob-on-hover ml-auto">
       <img
         src="../assets/placeholder-about.png"
         class="ml-auto"
@@ -63,16 +64,66 @@
         srcset="" />
     </div>
   </div>
+  <div
+    class="container mx-auto mt-24 flex flex-row px-24 max-md:flex-col max-md:px-4">
+    <div class="left w-1/2 max-md:w-full">
+      <h2 class="text-2xl font-medium text-black">
+        Habilidades <span class="text-purple">técnicas</span>
+      </h2>
+      <div class="mt-4 flex flex-row gap-x-10">
+        <div class="flex flex-col gap-y-3 text-black">
+          <p>Figma</p>
+          <p>Wordpress</p>
+          <p>HTML/CSS</p>
+        </div>
+        <div class="flex flex-col gap-y-3 text-black">
+          <p>SASS</p>
+          <p>Bootstrap/Bulma</p>
+          <p>Tailwind/DaisyUI</p>
+        </div>
+        <div class="flex flex-col gap-y-3 text-black">
+          <p>Javascript</p>
+          <p>Vuejs</p>
+          <p class="hidden">Nuxt/Typescript</p>
+        </div>
+      </div>
+    </div>
+    <div class="right">
+      <h2 class="text-2xl font-medium text-black">
+        Formação <span class="text-purple">relevante</span>
+      </h2>
+      <div class="mt-4">
+        <div class="unit">
+          <p class="font-medium text-black">
+            Bacharelado em Ciência e Tecnologia, UFRN
+          </p>
+          <p class="text-sm text-black">Jun 2021 / Cursando</p>
+        </div>
+        <div class="unit mt-4">
+          <p class="font-medium text-black">
+            Build Web Apps with Vuejs & Firebase, Shaun Peiling - Udemy
+          </p>
+          <p class="text-sm text-black">2022 / Completo</p>
+        </div>
+        <div class="unit mt-4">
+          <p class="font-medium text-black">
+            The Web Developer Bootcamp, Colt Steele - Udemy
+          </p>
+          <p class="text-sm text-black">2021 / Completo</p>
+        </div>
+        <div class="unit mt-4">
+          <p class="font-medium text-black">
+            Diploma de Ensino Médio, Contemporâneo
+          </p>
+          <p class="text-sm text-black">2017 - 2020 / Completo</p>
+        </div>
+      </div>
+    </div>
+  </div>
   <Footer />
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 90vh;
-  }
-}
-</style>
+<style></style>
 
 <script setup>
 import Header from '../components/Header.vue'
